63695632dca148b8e6cc3837e6018cd98c8be99c
Sabrinetj <zaghouanisabrine1030@gmail.com>
2024-04-11 16:13:32 +0200

    sorte lest

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function
+ * @list: list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t	*head;
+	listint_t	*tmp;
+
+	if (!list)
+		return;
+	if (!*list)
+		return;
+	head = *list;
+	while (head)
+	{
+		while (head->next && (head->n > head->next->n))
+		{
+			tmp = head->next;
+			head->next = tmp->next;
+			tmp->prev = head->prev;
+
+			if (head->prev)
+				head->prev->next = tmp;
+			if (tmp->next)
+				tmp->next->prev = head;
+
+			head->prev = tmp;
+			tmp->next = head;
+
+			if (tmp->prev)
+				head = tmp->prev;
+			else
+				*list = tmp;
+
+			print_list(*list);
+		}
+		head = head->next;
+	}
+}
+
